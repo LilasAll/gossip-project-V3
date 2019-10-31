1dddb1ee99cd89f3615a71dac5d6bb620f97abf5
LilasAll <lilas.allard@gmail.com>
2019-10-31 15:14:57 +0100

    first commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+	def new
+
+	end
+
+	def index
+
+	end
+
+	def create
+  # cherche s'il existe un utilisateur en base avec l’e-mail
+  user = User.find_by(email: params[:email])
+  puts"========================================================================================"
+  puts params
+  puts"========================================================================================"
+  # on vérifie si l'utilisateur existe bien ET si on arrive à l'authentifier (méthode bcrypt) avec le mot de passe
+		if user && user.authenticate(password: params[:password])
+	  	flash.now[:danger] = 'Bien inscrit!'
+	    session[:user_id] = user.id
+			redirect_to gossips_path
+	  else
+	    flash.now[:danger] = 'Invalid email/password combination'
+	    redirect_to welcome_index_path
+	  end
+	end
+
+	def destroy
+		session.delete(:user_id)
+	end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'comments/show'
+  resources :city
+  resources :gossip 
+  resources :author
+  resources :welcome, only: [:index, :show]
+  resources :contact, only: [:index]
+  resources :team, only: [:index]
+  resources :comment
+  resources :sessions, only: [:new, :create, :destroy]
+end
